df1abe76709a472cb28d190e9bb6b3d2df1969ce
tlek <tikenty@gmail.com>
2024-03-18 11:32:38 +0500

    added index.html, index.js and styles.css files in project

---
+++ index.js
@@ -0,0 +1,20 @@
+let timerId;
+
+const startButton = document.getElementById('start');
+startButton.addEventListener('click', function () {
+  timerId = setInterval(updateClock, 1000);
+});
+
+const stopButton = document.getElementById('stop');
+stopButton.addEventListener('click', function () {
+  clearInterval(timerId);
+});
+
+function updateClock() {
+  const clock = document.getElementById('clock');
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+  clock.textContent = hours + ':' + minutes + ':' + seconds;
+}
